eca4325d6b556f8acd21650af0f46d357c2f946b
luna <iszychen@gmail.com>
2021-04-10 14:08:37 +0800

    redis-spring-boot-starter

---
+++ src/main/java/com/luna/redis/redis/RedisApplication.java
@@ -0,0 +1,20 @@
+package com.luna.redis.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @Package: com.luna.message
+ * @ClassName: MessageApplication
+ * @Author: luna
+ * @CreateTime: 2020/7/16 16:54
+ * @Description:
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class RedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class);
+    }
+
+}
---
+++ src/main/java/com/luna/redis/redis/util/RedisKeyUtil.java
@@ -0,0 +1,183 @@
+package com.luna.redis.redis.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+/**
+ * redisKey设计
+ */
+@Component
+public class RedisKeyUtil {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    // =============================common============================
+    /**
+     * 指定缓存失效时间
+     *
+     * @param key 键
+     * @param time 时间(秒)
+     * @return
+     */
+    public boolean expire(String key, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.expire(key, time, TimeUnit.SECONDS);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 返回模糊匹配的key
+     * 
+     * @param key
+     * @return
+     */
+    public List<String> getKeys(String key) {
+        Set<String> keys = redisTemplate.keys(key);
+        return new ArrayList<>(keys);
+    }
+
+    /**
+     * 根据key 获取过期时间
+     *
+     * @param key 键 不能为null
+     * @return 时间(秒) 返回0代表为永久有效
+     */
+    public long getExpire(String key) {
+        return redisTemplate.getExpire(key, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 判断key是否存在
+     *
+     * @param key 键
+     * @return true 存在 false不存在
+     */
+    public boolean hasKey(String key) {
+        try {
+            return redisTemplate.hasKey(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 将key设置为永久有效
+     *
+     * @param key
+     */
+    public void persistKey(String key) {
+        redisTemplate.persist(key);
+    }
+
+    /**
+     * 删除缓存
+     *
+     * @param key 可以传一个值 或多个
+     */
+    @SuppressWarnings("unchecked")
+    public void del(String... key) {
+        if (key != null && key.length > 0) {
+            if (key.length == 1) {
+                redisTemplate.delete(key[0]);
+            } else {
+                redisTemplate.delete(CollectionUtils.arrayToList(key));
+            }
+        }
+    }
+
+    /**
+     * newKey不存在时才重命名
+     *
+     * @param oldKey
+     * @param newKey
+     * @return 修改成功返回true
+     */
+    public boolean renameKeyNotExist(String oldKey, String newKey) {
+        return redisTemplate.renameIfAbsent(oldKey, newKey);
+    }
+
+    /**
+     * 删除key
+     *
+     * @param key
+     */
+    public void deleteKey(String key) {
+        redisTemplate.delete(key);
+    }
+
+    /**
+     * 删除多个key
+     *
+     * @param keys
+     */
+    public void deleteKey(String... keys) {
+        Set<String> kSet = Stream.of(keys).map(k -> k).collect(Collectors.toSet());
+        redisTemplate.delete(kSet);
+    }
+
+    /**
+     * 删除Key的集合
+     *
+     * @param keys
+     */
+    public void deleteKey(Collection<String> keys) {
+        Set<String> kSet = keys.stream().map(k -> k).collect(Collectors.toSet());
+        redisTemplate.delete(kSet);
+    }
+
+    /**
+     * redis的key
+     * 形式为：
+     * 表名:主键名:主键值:列名
+     *
+     * @param tableName 表名
+     * @param majorKey 主键名
+     * @param majorKeyValue 主键值
+     * @param column 列名
+     * @return
+     */
+    public static String getKeyWithColumn(String tableName, String majorKey, String majorKeyValue, String column) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(tableName).append(":");
+        buffer.append(majorKey).append(":");
+        buffer.append(majorKeyValue).append(":");
+        buffer.append(column);
+        return buffer.toString();
+    }
+
+    /**
+     * redis的key
+     * 形式为：
+     * 表名:主键名:主键值
+     *
+     * @param tableName 表名
+     * @param majorKey 主键名
+     * @param majorKeyValue 主键值
+     * @return
+     */
+    public static String getKey(String tableName, String majorKey, String majorKeyValue) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(tableName).append(":");
+        buffer.append(majorKey).append(":");
+        buffer.append(majorKeyValue).append(":");
+        return buffer.toString();
+    }
+}
